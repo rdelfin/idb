28bc15af12f8aa0ea119fbd76d02949b9c761a9e
Xuming Zeng <xsznix@gmail.com>
2017-09-26 18:05:30 -0500

    Make a shitty home page

--- js/components/HomePage/HomePage.js
+++ js/components/HomePage/HomePage.js
@@ -1,11 +1,15 @@
 // @flow
 import React, {PureComponent} from 'react';
-import PlaceholderPage from '../PlaceholderPage';
+import './homePage.css';
 
 export default class HomePage extends PureComponent {
   render() {
     return (
-      <PlaceholderPage>HomePage</PlaceholderPage>
+      <div styleName="root">
+        <div styleName="title">Welcome to PhoneDB</div>
+        <div styleName="subtitle">The most extensive database of phones in the world*</div>
+        <div styleName="subsubtitle">* Terms and conditions apply</div>
+      </div>
     );
   }
 }
