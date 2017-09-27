3200eaca3030d05b57aa2d6de2dc1e607b11cf15
Xuming Zeng <xsznix@gmail.com>
2017-09-27 17:52:47 -0500

    Update headshots

--- js/components/AboutPage/AboutPage.js
+++ js/components/AboutPage/AboutPage.js
@@ -10,8 +10,8 @@ export default class AboutPage extends PureComponent {
         <TeamMemberCard name="Brian Cui" avatarUrl="http://placehold.it/200x200" blurb="I am the best developer ever" />
         <TeamMemberCard name="Jonathan Fisher" avatarUrl="/static/images/jonathan.png" blurb="I am the best developer ever" />
         <TeamMemberCard name="Matt Gmitro" avatarUrl="http://placehold.it/200x200" blurb="I am the best developer ever" />
-        <TeamMemberCard name="Ricardo Delfin" avatarUrl="http://placehold.it/200x200" blurb="I am the best developer ever" />
-        <TeamMemberCard name="Trey Gonsoulin" avatarUrl="http://placehold.it/200x200" blurb="I am the best developer ever" />
+        <TeamMemberCard name="Ricardo Delfin" avatarUrl="/static/images/rdelfin.png" blurb="I am the best developer ever" />
+        <TeamMemberCard name="Trey Gonsoulin" avatarUrl="/static/images/trey.png" blurb="I am the best developer ever" />
         <TeamMemberCard name="Xuming Zeng" avatarUrl="http://placehold.it/200x200" blurb="I am the best developer ever" />
       </div>
     );
