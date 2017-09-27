ecfb5195b1c680bdba9fed41ca75f8ba18903ce2
Xuming Zeng <xsznix@gmail.com>
2017-09-26 23:33:40 -0500

    Add rudimentary about page

--- js/components/AboutPage/AboutPage.js
+++ js/components/AboutPage/AboutPage.js
@@ -1,11 +1,19 @@
 // @flow
 import React, {PureComponent} from 'react';
-import PlaceholderPage from '../PlaceholderPage';
+import TeamMemberCard from '../TeamMemberCard';
+import './aboutPage.css';
 
 export default class AboutPage extends PureComponent {
   render() {
     return (
-      <PlaceholderPage>AboutPage</PlaceholderPage>
+      <div styleName="teamCards">
+        <TeamMemberCard name="Brian Cui" avatarUrl="http://placehold.it/200x200" blurb="I am the best developer ever" />
+        <TeamMemberCard name="Jonathan Fisher" avatarUrl="http://placehold.it/200x200" blurb="I am the best developer ever" />
+        <TeamMemberCard name="Matt Gmitro" avatarUrl="http://placehold.it/200x200" blurb="I am the best developer ever" />
+        <TeamMemberCard name="Ricardo Delfin" avatarUrl="http://placehold.it/200x200" blurb="I am the best developer ever" />
+        <TeamMemberCard name="Trey Gonsoulin" avatarUrl="http://placehold.it/200x200" blurb="I am the best developer ever" />
+        <TeamMemberCard name="Xuming Zeng" avatarUrl="http://placehold.it/200x200" blurb="I am the best developer ever" />
+      </div>
     );
   }
 }
---
+++ js/components/TeamMemberCard/index.js
@@ -0,0 +1,3 @@
+// @flow
+import TeamMemberCard from './TeamMemberCard';
+export default TeamMemberCard;
