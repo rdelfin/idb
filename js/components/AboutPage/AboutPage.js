ac4b6a66689e5d9077f6aec8c5cc285d11f82d21
Brian Cui <brian.cui@live.com>
2017-09-27 19:17:57 -0500

    Add Brian's picture to the About page

--- js/components/AboutPage/AboutPage.js
+++ js/components/AboutPage/AboutPage.js
@@ -10,7 +10,7 @@ export default class AboutPage extends PureComponent {
         <h1 styleName="title">About Us</h1>
         <p styleName="description">We are a team of UT students who are passionate about getting an A in SWE and graduating on time.</p>
         <div styleName="teamCards">
-          <TeamMemberCard name="Brian Cui" avatarUrl="http://placehold.it/200x200" blurb="I am the best developer ever" />
+          <TeamMemberCard name="Brian Cui" avatarUrl="/static/images/brian.png" blurb="I am the best developer ever" />
           <TeamMemberCard name="Jonathan Fisher" avatarUrl="/static/images/jonathan.png" blurb="I am the best developer ever" />
           <TeamMemberCard name="Matt Gmitro" avatarUrl="http://placehold.it/200x200" blurb="I am the best developer ever" />
           <TeamMemberCard name="Ricardo Delfin" avatarUrl="/static/images/rdelfin.png" blurb="I am the best developer ever" />
