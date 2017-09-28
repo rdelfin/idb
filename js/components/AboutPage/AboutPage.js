3d181c6fc5911a0bf74eeccd3c15097123e19316
Xuming Zeng <xsznix@gmail.com>
2017-09-28 16:46:20 -0500

    Add per-team member stats

--- js/components/AboutPage/AboutPage.js
+++ js/components/AboutPage/AboutPage.js
@@ -12,12 +12,60 @@ export default class AboutPage extends PureComponent {
         <h2 styleName="title2">Our Team</h2>
         <p styleName="description">We are a team of UT students who are passionate about getting an A in SWE and graduating on time.</p>
         <div styleName="teamCards">
-          <TeamMemberCard name="Brian Cui" avatarUrl="/static/images/brian.png" blurb="Born and raised in Austin. Working on a Master's degree. I fight for the user. I mean, I write user stories. I also write code." />
-          <TeamMemberCard name="Jonathan Fisher" avatarUrl="/static/images/jonathan.png" blurb="Born from the backwoods of Silsbee, TX. My family still thinks I'm doing an 'IT' job, but hey, I'm doing right by UT." />
-          <TeamMemberCard name="Matt Gmitro" avatarUrl="/static/images/MattGmitro.jpg" blurb="Born in Minneapolis, I migrated south and grew up in Flower Mound, TX. My focus is in preventing cryptocurrency bubbles from bursting." />
-          <TeamMemberCard name="Ricardo Delfin" avatarUrl="/static/images/rdelfin.png" blurb="Tapatio who grew up in Mexico City! Interested in graphics, C++, systems, and anything that involves as little node.js as possible." />
-          <TeamMemberCard name="Trey Gonsoulin" avatarUrl="/static/images/trey.png" blurb="21+ years in Austin. I'm still skeptical, but the others assure me this 'internet' thing will really take off, so I'm along for the ride." />
-          <TeamMemberCard name="Xuming Zeng" avatarUrl="/static/images/xsznix.jpg" blurb="Born in PRC, raised in AB, FL, CO, and TX. I didn't expect to learn anything from this, but now I know that I hate Webpack." />
+          <TeamMemberCard
+            name="Brian Cui"
+            avatarUrl="/static/images/brian.png"
+            blurb="Born and raised in Austin. Working on a Master's degree. I fight for the user. I mean, I write user stories. I also write code."
+            tasks="I did absolutely nothing on this project."
+            commits={3}
+            issues={5}
+            tests={0}
+            />
+          <TeamMemberCard
+            name="Jonathan Fisher"
+            avatarUrl="/static/images/jonathan.png"
+            blurb="Born from the backwoods of Silsbee, TX. My family still thinks I'm doing an 'IT' job, but hey, I'm doing right by UT."
+            tasks="I did absolutely nothing on this project."
+            commits={3}
+            issues={5}
+            tests={0}
+            />
+          <TeamMemberCard
+            name="Matt Gmitro"
+            avatarUrl="/static/images/MattGmitro.jpg"
+            blurb="Born in Minneapolis, I migrated south and grew up in Flower Mound, TX. My focus is in preventing cryptocurrency bubbles from bursting."
+            tasks="I did absolutely nothing on this project."
+            commits={4}
+            issues={2}
+            tests={0}
+            />
+          <TeamMemberCard
+            name="Ricardo Delfin"
+            avatarUrl="/static/images/rdelfin.png"
+            blurb="Tapatio who grew up in Mexico City! Interested in graphics, C++, systems, and anything that involves as little node.js as possible."
+            tasks="I did absolutely nothing on this project."
+            commits={11}
+            issues={7}
+            tests={0}
+            />
+          <TeamMemberCard
+            name="Trey Gonsoulin"
+            avatarUrl="/static/images/trey.png"
+            blurb="21+ years in Austin. I'm still skeptical, but the others assure me this 'internet' thing will really take off, so I'm along for the ride."
+            tasks="I did absolutely nothing on this project."
+            commits={20}
+            issues={5}
+            tests={0}
+            />
+          <TeamMemberCard
+            name="Xuming Zeng"
+            avatarUrl="/static/images/xsznix.jpg"
+            blurb="Born in PRC, raised in AB, FL, CO, and TX. I didn't expect to learn anything from this, but now I know that I hate Webpack."
+            tasks="I did absolutely nothing on this project."
+            commits={27}
+            issues={8}
+            tests={0}
+            />
         </div>
         <h2 styleName="title2">Our Tools</h2>
         <p styleName="description">Our artisan code-crafting process leverages cutting-edge industry best practices and a powerful toolchain built on the latest technologies.</p>
@@ -26,12 +74,12 @@ export default class AboutPage extends PureComponent {
           <div styleName="aboutContent">
             <h3 styleName="aboutHeader"><b>GitHub</b>: Source Control <a href="https://github.com/rdelfin/idb" styleName="link" className="fa fa-external-link" target="_blank" /></h3>
             <p>We used GitHub for source control, allowing us all to work on the same code at the same time. Git provides a complete history of our codebase, a way to easily resolve merge conflicts, and a way to figure out who broke the build.</p>
-            <p styleName="stats">
+            <div styleName="stats">
               <div>Commits: 76</div>
               <div>Pull requests: 1</div>
               <div>React components: 19</div>
               <div>Unit tests: 0</div>
-            </p>
+            </div>
           </div>
         </div>
         <div styleName="aboutCard">
@@ -39,11 +87,11 @@ export default class AboutPage extends PureComponent {
           <div styleName="aboutContent">
             <h3 styleName="aboutHeader"><b>Trello</b>: Issue Tracking <a href="https://trello.com/b/eYMtMgrg" styleName="link" className="fa fa-external-link" target="_blank" /></h3>
             <p>We used Trello to keep track of what we are working on and what we need to complete before the project deadline.</p>
-            <p styleName="stats">
+            <div styleName="stats">
               <div>Open issues: 10</div>
               <div>Closed issues: 13</div>
               <div>Resource links: 8</div>
-            </p>
+            </div>
           </div>
         </div>
         <div styleName="aboutCard">
@@ -51,10 +99,10 @@ export default class AboutPage extends PureComponent {
           <div styleName="aboutContent">
             <h3 styleName="aboutHeader"><b>Apiary</b>: API Documentation <a href="http://docs.rdelfin.apiary.io" styleName="link" className="fa fa-external-link" target="_blank" /></h3>
             <p>Even though we don't have an API yet, we used Apiary to document what our API should eventually look like once we implement it.</p>
-            <p styleName="stats">
+            <div styleName="stats">
               <div>Documented endpoints: 4</div>
               <div>Implemented endpoints: 0</div>
-            </p>
+            </div>
           </div>
         </div>
         <div styleName="aboutCard">
@@ -62,9 +110,9 @@ export default class AboutPage extends PureComponent {
           <div styleName="aboutContent">
             <h3 styleName="aboutHeader"><b>NPM</b>: Package Management</h3>
             <p>Also known as the Node Package Manager, we downloaded many of our development tools using this standard frontend development tool, including <b>React, React Router, Babel, Webpack, Flow, CSS Loader, and SASS</b>.</p>
-            <p styleName="stats">
+            <div styleName="stats">
               <div>Dependencies installed: 597</div>
-            </p>
+            </div>
           </div>
         </div>
       </div>
--- js/components/TeamMemberCard/TeamMemberCard.js
+++ js/components/TeamMemberCard/TeamMemberCard.js
@@ -5,13 +5,28 @@ type Props = {
   name: string,
   avatarUrl: string,
   blurb: string,
+  tasks: string,
+  commits: number,
+  issues: number,
+  tests: number,
 };
 
 const TeamMemberCard = (props: Props): React$Element<*> => (
   <div styleName="root">
-    <img styleName="avatar" src={props.avatarUrl} />
-    <div styleName="name">{props.name}</div>
-    <div styleName="blurb">{props.blurb}</div>
+    <div styleName="front">
+      <img styleName="avatar" src={props.avatarUrl} />
+      <div styleName="name">{props.name}</div>
+      <div styleName="blurb">{props.blurb}</div>
+    </div>
+    <div styleName="back">
+      <h4 styleName="header">My responsibilities</h4>
+      <div>{props.tasks}</div>
+      <div styleName="stats">
+        <div>Commits: {props.commits}</div>
+        <div>Issues: {props.issues}</div>
+        <div>Unit tests: {props.tests}</div>
+      </div>
+    </div>
   </div>
 );
 
