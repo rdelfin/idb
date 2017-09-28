777981c1f504de6416e56f647022546bc588b285
Brian Cui <brian.cui@live.com>
2017-09-27 19:34:05 -0500

    Filled out Brian's bio

--- js/components/AboutPage/AboutPage.js
+++ js/components/AboutPage/AboutPage.js
@@ -10,7 +10,7 @@ export default class AboutPage extends PureComponent {
         <h1 styleName="title">About Us</h1>
         <p styleName="description">We are a team of UT students who are passionate about getting an A in SWE and graduating on time.</p>
         <div styleName="teamCards">
-          <TeamMemberCard name="Brian Cui" avatarUrl="/static/images/brian.png" blurb="I am the best developer ever" />
+          <TeamMemberCard name="Brian Cui" avatarUrl="/static/images/brian.png" blurb="Born and raised in Austin. Working on a Master's degree. I fight for the user. I mean, I write user stories. I also write code." />
           <TeamMemberCard name="Jonathan Fisher" avatarUrl="/static/images/jonathan.png" blurb="I am the best developer ever" />
           <TeamMemberCard name="Matt Gmitro" avatarUrl="http://placehold.it/200x200" blurb="I am the best developer ever" />
           <TeamMemberCard name="Ricardo Delfin" avatarUrl="/static/images/rdelfin.png" blurb="I am the best developer ever" />
