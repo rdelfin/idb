80e6600d197c0ef3d12134631f0aaf66241a0340
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-10-25 18:24:50 -0500

    Implemented get_carrier_all()

--- app/db_interface.py
+++ app/db_interface.py
@@ -20,4 +20,23 @@ class Database:
         pass
 
     def get_carrier_all(self):
-        pass
+        carriers = []
+        with self.Session() as session:
+            for carrier in session.query(tables.Carrier).order_by(tables.Carrier.id).all():
+
+                c_brands = session.query(tables.CarrierBrand).filter_by(carrier_id=carrier.id) \
+                                .join(tables.Brand).all()
+                c_models = session.query(tables.CarrierModel).filter_by(carrier_id=carrier.id) \
+                                .join(tables.Model).all()
+
+                brand_names = [brand.name for brand in c_brands]
+                model_names = [model.name for model in c_models]
+
+                new_carrier = models.Carrier(carrier.image, carrier.name,
+                                             carrier.short_name, carrier.cellular_networks,
+                                             carrier.covered_countries,
+                                             brand_names, model_names)
+
+                carriers += new_carrier
+
+        return carriers
