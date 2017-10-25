dc95fb2612f4c0ac22e51ea9c129d8f93b3c79d5
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-10-25 18:15:27 -0500

    Added new models for phone

--- app/models.py
+++ app/models.py
@@ -1,5 +1,88 @@
 # This is where all of our model classes are defined
 
+class PhysicalAttributes:
+    def __init__(self, width, height, depth, dimensions, mass):
+        self.width = width
+        self.height = height
+        self.depth = depth
+        self.dimensions = dimensions
+        self.mass = mass
+
+
+class Software:
+    def __init__(self, platform, os, software_extras):
+        self.platform = platform
+        self.os = os
+        self.software_extras = software_extras
+
+class Hardware:
+    def __init__(self, cpu, gpu, ram, nonvolatile_memory):
+        self.cpu = cpu
+        self.gpu = gpu
+        self.ram = ram
+        self.nonvolatile_memory = nonvolatile_memory
+
+class Cpu:
+    def __init__(self, model, additional_info, clock_speed):
+        self.model = model
+        self.additional_info = additional_info
+        self.clock_speed = clock_speed
+
+
+class Gpu:
+    def __init__(self, model, clock_speed):
+        self.model = model
+        self.clock_speed = clock_speed
+
+
+class Ram:
+    def __init__(self, type_m, capacity):
+        self.type_m = type_m
+        self.capacity = capacity
+
+
+class NonvolatileMemory:
+    def __init__(self, type_m, capacity):
+        self.type_m = type_m
+        self.capacity = capacity
+
+
+class Display:
+    def __init__(self, resolution, diagonal, width, height, bezel_width, \
+                 area_utilization, pixel_density, type_m, color_depth, screen):
+         self.resolution = resolution
+         self.diagonal = diagonal
+         self.width = width
+         self.height = height
+         self.bezel_width = bezel_width
+         self.area_utilization = area_utilization
+         self.pixel_density = pixel_density
+         self.type_m = type_m
+         self.color_depth = color_depth
+         self.screen = screen
+
+class Camera:
+    def __init__(self, placement, module, sensor, sensor_format, resolution, \
+                 num_pixels, aperture, optical_zoom, digital_zoom, focus, \
+                 camcorder, flash):
+        self.placement = placement
+        self.module = module
+        self.sensor = sensor
+        self.sensor_format = sensor_format
+        self.resolution = resolution
+        self.num_pixels = num_pixels
+        self.aperture = aperture
+        self.optical_zoom = optical_zoom
+        self.digital_zoom = digital_zoom
+        self.focus = focus
+        self.camcorder = camcorder
+        self.flash = flash
+
+class Camcorder:
+    def __init__(self, resolution, formats):
+        self.resolution = resolution
+        self.formats = formats
+
 class Model:
     def __init__(self, image, name, brand, model, release_date,
                  hardware_designer, manufacturers, codename, market_countries,
