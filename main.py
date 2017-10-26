4882ad0757b6121bf0a7c4f0f5d922a4860e6761
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-10-25 21:09:16 -0500

    Added all endpoints

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
+
+from app import db_interface
 
 @app.route('/')
 def hello_world():
@@ -7,19 +9,19 @@ def hello_world():
 
 @app.route('/models', methods=['GET'])
 def models_get():
-    raise NotImplementedError
+    return jsonify(db_interface.get_os_all())
 
 @app.route('/brands', methods=['GET'])
 def brands_get():
-    raise NotImplementedError
+    return jsonify(db_interface.get_brand_all())
 
 @app.route('/os', methods=['GET'])
 def os_get():
-    raise NotImplementedError
+    return jsonify(db_interface.get_os_all())
 
 @app.route('/carriers', methods=['GET'])
 def carriers_get():
-    raise NotImplementedError
+    return jsonify(db_interface.get_carrier_all())
 
 if __name__ == '__main__':
   app.run(debug=True)
