270808bb3cc932b1ad7ab88251eee47529deeae5
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-10-25 21:15:32 -0500

    Fixed endpoints to use instance of Databse class

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
-from app import db_interface
+from app.db_interface import Database
+
+db = Database()
 
 @app.route('/')
 def hello_world():
@@ -9,19 +11,19 @@ def hello_world():
 
 @app.route('/models', methods=['GET'])
 def models_get():
-    return jsonify(db_interface.get_model_all())
+    return jsonify(db.get_model_all())
 
 @app.route('/brands', methods=['GET'])
 def brands_get():
-    return jsonify(db_interface.get_brand_all())
+    return jsonify(db.get_brand_all())
 
 @app.route('/os', methods=['GET'])
 def os_get():
-    return jsonify(db_interface.get_os_all())
+    return jsonify(db.get_os_all())
 
 @app.route('/carriers', methods=['GET'])
 def carriers_get():
-    return jsonify(db_interface.get_carrier_all())
+    return jsonify(db.get_carrier_all())
 
 if __name__ == '__main__':
   app.run(debug=True)
