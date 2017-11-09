614f79a8bf0f63d3849faaf022947b8f50744454
Brian Cui <brian.cui@live.com>
2017-11-08 20:04:50 -0600

    Fixed attempted insertion for os into brands

--- pickle_inserter.py
+++ pickle_inserter.py
@@ -4,7 +4,7 @@ import pickle
 models = pickle.load(open('phones.pickle', 'rb'))
 carriers = pickle.load(open('carriers.pickle', 'rb'))
 oss = pickle.load(open('oss.pickle', 'rb'))
-brands = pickle.load(open('oss.pickle', 'rb'))
+brands = pickle.load(open('brands.pickle', 'rb'))
 
 app.inserter.insert_all(models=models, carriers=carriers, oss=oss, brands=brands)
 
