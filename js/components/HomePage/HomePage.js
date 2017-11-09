ea8cfbe772091b2fd6ae9b299691cac53d4cddf6
Xuming Zeng <xsznix@gmail.com>
2017-11-09 17:59:47 -0600

    Add a background image for the home page. Source: http://www.bestproducts.com/tech/gadgets/news/g1800/phones-for-kids/

--- js/components/HomePage/HomePage.js
+++ js/components/HomePage/HomePage.js
@@ -4,15 +4,18 @@ import {Link} from 'react-router-dom';
 import './homePage.css';
 
 const HomePage = () => (
-  <div styleName="root">
-    <div styleName="title">Welcome to PhoneDB</div>
-    <div styleName="subtitle">The most extensive database of phones in the world*</div>
-    <div styleName="subsubtitle">* Terms and conditions apply</div>
-    <div styleName="browseTable">
-      <Link to="/phones" styleName="browseLink">Models <i styleName="browseCaret" className="fa fa-caret-right" /></Link>
-      <Link to="/manufacturers" styleName="browseLink">Manufacturers <i styleName="browseCaret" className="fa fa-caret-right" /></Link>
-      <Link to="/carriers" styleName="browseLink">Carriers <i styleName="browseCaret" className="fa fa-caret-right" /></Link>
-      <Link to="/os" styleName="browseLink">OSs <i styleName="browseCaret" className="fa fa-caret-right" /></Link>
+  <div>
+    <div styleName="bg" />
+    <div styleName="root">
+      <div styleName="title">Welcome to PhoneDB</div>
+      <div styleName="subtitle">The most extensive database of phones in the world*</div>
+      <div styleName="subsubtitle">* Terms and conditions apply</div>
+      <div styleName="browseTable">
+        <Link to="/phones" styleName="browseLink">Models <i styleName="browseCaret" className="fa fa-caret-right" /></Link>
+        <Link to="/manufacturers" styleName="browseLink">Manufacturers <i styleName="browseCaret" className="fa fa-caret-right" /></Link>
+        <Link to="/carriers" styleName="browseLink">Carriers <i styleName="browseCaret" className="fa fa-caret-right" /></Link>
+        <Link to="/os" styleName="browseLink">OSs <i styleName="browseCaret" className="fa fa-caret-right" /></Link>
+      </div>
     </div>
   </div>
 );
