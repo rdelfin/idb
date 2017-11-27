0cf570dc66f14dcc4d55e708f52bfec8072e6abd
Xuming Zeng <xsznix@gmail.com>
2017-11-27 16:13:42 -0600

    Increase speed of API requests substantially

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, jsonify
+from functools import lru_cache
 import os
 app = Flask(__name__)
 
@@ -7,26 +8,32 @@ from app.db_interface import Database
 db = Database()
 
 @app.route('/')
+@lru_cache(maxsize=1)
 def hello_world():
     return render_template('index.html')
 
 @app.route('/models', methods=['GET'])
+@lru_cache(maxsize=1)
 def models_get():
     return jsonify([model.serialize() for model in db.get_model_all()])
 
 @app.route('/brands', methods=['GET'])
+@lru_cache(maxsize=1)
 def brands_get():
     return jsonify([brand.serialize() for brand in db.get_brand_all()])
 
 @app.route('/os', methods=['GET'])
+@lru_cache(maxsize=1)
 def os_get():
     return jsonify([os.serialize() for os in db.get_os_all()])
 
 @app.route('/carriers', methods=['GET'])
+@lru_cache(maxsize=1)
 def carriers_get():
     return jsonify([carrier.serialize() for carrier in db.get_carrier_all()])
 
 @app.route('/models/<id>')
+@lru_cache(maxsize=1024)
 def model_id_get(id):
     model = db.get_model_id(id)
     if model is None:
@@ -34,6 +41,7 @@ def model_id_get(id):
     return jsonify(model.serialize())
 
 @app.route('/brands/<id>')
+@lru_cache(maxsize=1024)
 def brands_id_get(id):
     brand = db.get_brand_id(id)
     if brand is None:
@@ -41,6 +49,7 @@ def brands_id_get(id):
     return jsonify(brand.serialize())
 
 @app.route('/os/<id>')
+@lru_cache(maxsize=1024)
 def os_id_get(id):
     os = db.get_os_id(id)
     if os is None:
@@ -48,6 +57,7 @@ def os_id_get(id):
     return jsonify(os.serialize())
 
 @app.route('/carriers/<id>')
+@lru_cache(maxsize=1024)
 def carriers_id_get(id):
     carrier = db.get_model_id(id)
     if carrier is None:
